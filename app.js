af810666651ccfc2a9c68ce1f08a881199f4fba0
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-08-27 19:20:21 +0600

    finallize

--- app.js
+++ app.js
@@ -1,54 +1,40 @@
-function fetchApiServer(url=''){
+// function fetchApiServer(url=''){
     
-    url ='https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2Fshafiq-portfolio.netlify.app%2Fecommerce-website.html&device=phone&dimension=480x800&format=png';
-    // url ='https://api.screenshotmachine.com/?key=8beab1&url=&dimension=1024x768';
-    fetch(url)
-    .then(res => displayPreview(res.url))
-    // .then(res => console.log(res.url))
-    // .then(res => res.json())
-    // .then(jsonData => displayPreview(jsonData));
-    // console.log(url);
-}
-// fetchApiServer();
-function displayPreview(imgUrl='shafiq-portfolio.netlify.app/ecommerce-website.html'){
-    const imageContainer = document.getElementById('image-container')
-    // create a img element 
-    // const newImgDiv = document.createElement('img')
-    const filtered_url = imgUrl.replace('/','%2F')
-    // const newImg = `<img src="https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2F${filtered_url}&device=phone&dimension=480x800&format=png" alt="" height="400px">`
-
-
-// https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2Fshafiq-portfolio.netlify.app%2ecommerce-website.html&device=phone&dimension=480x800&format=png
-// https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2Fshafiq-portfolio.netlify.app%2Fecommerce-website.html&device=phone&dimension=480x800&format=png
-
-
-    const newImg2 = document.createElement('img')
-    // newImg2.setAttribute('src',`https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2F${filtered_url}&device=phone&dimension=480x800&format=png`)
-    // newImg2.setAttribute('height',400)
-    // imageContainer.appendChild(newImg2)
-    // console.log(imageContainer)
-    // console.log(`https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2F${filtered_url}&device=phone&dimension=480x800&format=png`)
-
-    const addImage = document.createElement('div')
-    // addImage.innerHTML=`<div class="d-flex flex-column align-items-center mx-2" style="">
-    //     <img src="https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2Fshafiq-portfolio.netlify.app%2Fecommerce-website.html&device=phone&dimension=480x800&format=png" alt="" width="300px";height="400";>
-    //     <button class="px-3 py-2 bg-info rounded mt-5 text-white">${imgUrl}</button>
-    // </div>`
-    addImage.innerHTML= `<div class="parent ">
+//     url ='https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2Fshafiq-portfolio.netlify.app%2Fecommerce-website.html&device=phone&dimension=480x800&format=png'
+//     fetch(url)
+//     .then(res => displayPreview(res.url))
     
-    <div class="d-flex flex-column align-items-center position-relative" >
-            <img src="loader.gif" alt="" srcset="" class="position-absolute" width="300px";height="400">
-            <img class="position-relative" src="https://api.screenshotmachine.com?key=8beab1&url=https%3A%2F%2Fshafiq-portfolio.netlify.app%2Fecommerce-website.html&device=phone&dimension=480x800&format=png" alt="" width="300px";height="400";>
-            <p>shafiq-portfolio.netlify.app</p>
-        </div>
-    </div>`
-    imageContainer.appendChild(addImage)
-
+// }
+function startWithHttp(url){
+    console.log(url.search('http://'))
+   if(url.search('http://')==0 || url.search('https://')==0){
+       console.log(url, 'found')
+   }else{
+    console.log(url, 'not found', 'http://'+url )
+   }
 }
+function eventForEnter(event){
+    console.log(event.key)
+    if(event.key == 'Enter'){
+        displayScreenshot()
+    }
+}
+// shafiq-portfolio.netlify.app/ecommerce-website.html
+function displayScreenshot (imgUrl=''){
+    
+    const websiteUrlInput = document.getElementById('website-url-input');
+    if(websiteUrlInput.value){
+        imgUrl = websiteUrlInput.value ; 
+    }else{
+        return;
+    }
 
-function displayScreenshot (imgUrl='shafiq-portfolio.netlify.app/ecommerce-website.html'){
-
-
+    startWithHttp(imgUrl);
+    // return;
+    // console.log(imgUrl)
+    // const validateUrl = isValidHttpUrl(imgUrl);
+    // console.log(validateUrl);
+    // return;
     // get radio button value 
     const deviceSelect = document.getElementsByName('device');
     let selectedDevice = '';
@@ -80,14 +66,12 @@ function displayScreenshot (imgUrl='shafiq-portfolio.netlify.app/ecommerce-websi
     // &device=desktop&dimension=1024x768
 
 
-    const websiteUrlInput = document.getElementById('website-url-input');
-    if(websiteUrlInput.value){
-        imgUrl = websiteUrlInput.value ; 
-    }
+   
     
 
     const imageContainer = document.getElementById('image-container')
-    const filtered_url = imgUrl.replace('/','%2F')
+    let filtered_url = imgUrl.replace('/','%2F')
+    filtered_url = filtered_url.replace(':','%3A')
 
     const addImage = document.createElement('div')
     addImage.innerHTML= `<div class="parent content-bg ">
@@ -98,4 +82,6 @@ function displayScreenshot (imgUrl='shafiq-portfolio.netlify.app/ecommerce-websi
         </div>
     </div>`
     imageContainer.appendChild(addImage)
+
+    websiteUrlInput.value = '';
 }
